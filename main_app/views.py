308bccaadf62b91825421aa82ce8ba7ee9fac209
Nicole Baird <nbaird44@gmail.com>
2022-02-21 20:51:11 -0500

    stop foreign key drop down limited to user's created trips

--- main_app/views.py
+++ main_app/views.py
@@ -55,6 +55,10 @@ class StopCreate(LoginRequiredMixin, CreateView):
     model = Stop
     fields = '__all__'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'].fields['trip'].queryset = Trips.objects.filter(user=self.request.user)    
+        return context
 
 class StopUpdate(LoginRequiredMixin, UpdateView):
     model = Stop
